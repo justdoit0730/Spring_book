5d61fdd93a76fe5a3f14f811cc111b8317cb252c
junhee <a07308@naver.com>
2022-05-02 21:48:05 +0900

    jex00_1

---
+++ jex00/src/test/java/org/zerock/controller/WebConfig.java
@@ -0,0 +1,24 @@
+package org.zerock.controller;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends
+AbstractAnnotationConfigDispatcherServletInitializer{
+	
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses(){
+		return new Class[] {RootConfig.class};
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses(){
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
